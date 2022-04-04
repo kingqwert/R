6e9653d25b63744db98cf884ada47f165bb3009d
Daisuke Yoneoka <blue.sky.sea.dy@gmail.com>
2022-04-04 12:38:36 +0900

    Create Combine_LP folder

---
+++ Combine_LP/combine_lp_case1.R
@@ -0,0 +1,201 @@
+library(Matrix)
+library(MASS)
+library(ROCR)
+library(tidyverse)
+library(qpcR)
+library(ggplot2)
+library(clusterGeneration)
+library(rms)
+
+
+# to prepare AUC, mean vectors, Sigma matrix from logits
+res.logits = function(x,y){
+	fit = glm(y~x,family="binomial")
+	#fit = lda(y ~ x)
+
+	AUC = prediction(fitted(fit), y) %>%
+  	performance(measure = "auc")%>%
+  	.@y.values %>% as.numeric
+
+  	return(list(
+		beta=coef(fit)[-1], # coefs
+		AUC.k=AUC,
+  		mu1 = apply(x[y==1,],2,mean,na.rm=T),
+  		mu0 = apply(x[y==0,],2,mean,na.rm=T),
+  		mu=apply(x[y==1,],2,mean,na.rm=T)-apply(x[y==0,],2,mean,na.rm=T),
+  		sd1 = apply(x[y==1,],2,sd,na.rm=T),
+  		sd0 = apply(x[y==0,],2,sd,na.rm=T)
+  		))
+}
+
+
+make.studies = function(N1,N0,m1,m0,Sigma1,Sigma0,cutoff1){	
+ 	# simulate design matrix
+	X.g1 = mvrnorm(N1,mu=m1,Sigma=Sigma1)
+	X.g0 = mvrnorm(N0,mu=m0,Sigma=Sigma0)
+	
+	
+	tmp = data.frame(
+		y=c(rep(0,N0),rep(1,N1)),
+		x=rbind(X.g0,X.g1))
+	
+	tmp$x.4 = ifelse(tmp$x.4>cutoff1,1,0)
+	#tmp$x.5 = ifelse(tmp$x.5>cutoff2,1,0)
+	return(tmp)
+}
+
+#######################################
+# Prepare for calculating AUC using our method and conventional methods
+#######################################
+AUC_total_our = matrix(NA,ncol=5,nrow=100)
+AUC_total_mean = matrix(NA,ncol=5,nrow=100)
+AUC_total_study1 =matrix(NA,ncol=5,nrow=100)
+AUC_total_study2 =matrix(NA,ncol=5,nrow=100)
+AUC_total_all = matrix(NA,ncol=5,nrow=100)
+
+# simulation parameters
+cors = c(-0.3,-0.1,0.1,0.3,0.6) 
+N1=N0=500
+
+for(c in 1:5){
+	for(sim in 1:100){
+		studies =list()
+		data.list =list()
+		Ck=list()
+		
+		mu.g1 = c(rep(1,5),rep(0.5,5))
+		mu.g0 = rep(0,10)
+		
+		# correlation matrix
+		cor=0.5
+		cor_studies = cors[c]
+		R = matrix(rep(cor,10^2), ncol=10) 
+		# R12
+		R[c(1:3),c(4:6)]=cor_studies
+		R[c(4:6),c(1:3)]=cor_studies
+		# R13
+		R[c(1:3),c(7:10)]=cor_studies
+		R[c(7:10),c(1:3)]=cor_studies
+		# R23
+		R[c(4:6),c(7:10)]=cor_studies
+		R[c(7:10),c(4:6)]=cor_studies
+		diag(R) = 1 
+
+		
+		# prepare Sigma
+		Sigma1 = diag(c(1:10)) %*% R %*% diag(c(1:10))
+
+		
+		# simulate design matrix
+		X.g1 = mvrnorm(N1,mu=mu.g1,Sigma=Sigma1)
+		X.g0 = mvrnorm(N0,mu=mu.g0,Sigma=Sigma1)
+		
+		test=data.frame(
+			y=c(rep(0,N0),rep(1,N1)),
+			x=rbind(X.g0,X.g1))
+		
+		test$x.4 = ifelse(test$x.4>1,1,0)
+		#test$x.5 = ifelse(test$x.5>1,1,0)
+		test$x.9 = ifelse(test$x.9>1,1,0)
+		#test$x.10 = ifelse(test$x.10>1,1,0)
+
+		Sigma_test = var(test[,-1])
+		
+	
+		# number of variables in each study
+		K=2
+		
+		for(i in 1:K){
+			if(i==1){
+				data.list[[i]]=make.studies(N1=500,N0=500,m1=mu.g1[1:5],m0=mu.g0[1:5], Sigma1=Sigma1[c(1:5),c(1:5)], Sigma0=Sigma1[c(1:5),c(1:5)],cutoff1=1)
+			}else if(i==2){
+				data.list[[i]]=make.studies(N1=500,N0=500,m1=mu.g1[6:10],m0=mu.g0[6:10], Sigma1=Sigma1[c(6:10),c(6:10)], Sigma0=Sigma1[c(6:10),c(6:10)],cutoff1=1)
+			}
+			
+			studies[[i]] = res.logits(as.matrix(data.list[[i]][,-1]),data.list[[i]]$y)
+		}
+		
+
+
+		
+		# calculate w_opt
+		B0hat = cbind(
+			c(studies[[1]]$beta,rep(0,5)),
+			c(rep(0,5),studies[[2]]$beta)
+		)
+		muhat = cbind(
+			c(studies[[1]]$mu,rep(NA,5)),
+			c(rep(NA,5),studies[[2]]$mu)
+		)
+
+		delta_hat = apply(muhat,1,mean,na.rm=T)
+		
+		
+		w_opt2 = c(solve(t(B0hat)%*%Sigma_test%*%B0hat)%*%t(B0hat)%*%delta_hat)
+		
+		final_opt2=list()
+		for(i in 1:K){
+			betas = studies[[i]]$beta*w_opt2[i]
+			if(i==1){
+				final_opt2[[i]] = c(betas,rep(0,5))	
+			}else if(i==2){
+				final_opt2[[i]] = c(rep(0,5),betas)	
+			}
+		}
+	
+
+		###############
+		# Comparison
+		###############
+		# let's compare
+		#our model
+		score=list()
+		for(i in 1:K){
+			score[[i]] = as.matrix(test[,-1]) %*%final_opt2[[i]]
+		}
+		
+		pred.our = Reduce('+',score) 
+		AUC_total_our[sim,c]=prediction(pred.our, test$y) %>%
+		  	performance(measure = "auc")%>%
+		  	.@y.values %>% as.numeric
+		
+		
+		#comparison groyup 1: each models
+		score.each=list()
+		phat_each=list()
+		AUC.each = numeric(K)
+		for(i in 1:K){
+			if(i==1){
+				beta = c(studies[[i]]$beta,rep(0,5))	
+			}else if(i==2){
+				beta = c(rep(0,5),studies[[i]]$beta)	
+			}
+			
+			score.each[[i]] =as.matrix(test[,-1]) %*%beta
+			
+			AUC.each[i]=prediction(score.each[[i]], test$y) %>%
+		  		performance(measure = "auc")%>%
+		  		.@y.values %>% as.numeric
+		}
+	
+		# AUC_total_each_max[sim,c]=max(AUC.each)
+		# AUC_total_each_mean[sim,c]=mean(AUC.each)
+		AUC_total_study1[sim,c]=AUC.each[1]
+		AUC_total_study2[sim,c]=AUC.each[2]
+
+		# comparison group 2: just taking mean of f_k
+		B0hat2 =  cbind(
+			c(studies[[1]]$beta,rep(NA,5)),
+			c(rep(NA,5),studies[[2]]$beta)
+		)
+		mean.coef = apply(B0hat2,1,mean,na.rm=T)
+		mean.score = as.matrix(test[,-1]) %*%mean.coef
+
+		
+		AUC_total_mean[sim,c]=prediction(mean.score, test$y) %>%
+		  		performance(measure = "auc")%>%
+		  		.@y.values %>% as.numeric
+
+	}
+}
+
